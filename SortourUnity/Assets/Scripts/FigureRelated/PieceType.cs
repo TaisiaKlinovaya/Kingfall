55211ec30a3eab2c2f3f84959ba1307287b7a4c2
chillmal <chillmal546@gmail.com>
2024-12-17 12:59:01 +0100

    Update PieceType.cs
    Bewegung vorbereitet!
    Aufruf der Methode folgt

--- SortourUnity/Assets/Scripts/FigureRelated/PieceType.cs
+++ SortourUnity/Assets/Scripts/FigureRelated/PieceType.cs
@@ -23,4 +23,29 @@ public class PieceType : MonoBehaviour
 
     private Vector3 desiredPosition;
     private Vector3 desiredScale;
+
+    private void Update()
+    {
+        //transform.position = Vector3.Lerp(transform.position, desiredPosition, Time.deltaTime * 10);
+        //transform.localScale = Vector3.Lerp(transform.localScale, desiredScale, Time.deltaTime * 10);
+    }
+
+    public virtual void SetPosition(Vector3 position, bool force = false)
+    {
+        desiredPosition = position;
+        if (force)
+        {
+            transform.position = desiredPosition;
+        }
+    }
+
+    public virtual void SetScale(Vector3 scale, bool force = false)
+    {
+        desiredScale = scale;
+        if (force)
+        {
+            transform.localScale = desiredScale;
+        }
+    }
 }
+
