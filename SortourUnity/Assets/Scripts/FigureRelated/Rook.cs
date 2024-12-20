817a74016960869a35cd26bfaad9952d685e66e4
chillmal <chillmal546@gmail.com>
2024-12-20 13:41:36 +0100

    change

--- SortourUnity/Assets/Scripts/FigureRelated/Rook.cs
+++ SortourUnity/Assets/Scripts/FigureRelated/Rook.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 
 public class Rook : PieceType
 {
-    private int currentX;
-    private int currentY;
+    //private int currentX;
+    //private int currentY;
 
-    public void GetPossibleMoves(int currentX, int currentY)
-    {
-        this.currentX = currentX;
-        this.currentY = currentY;
+    //public void GetPossibleMoves(int currentX, int currentY)
+    //{
+    //    this.currentX = currentX;
+    //    this.currentY = currentY;
 
-        Debug.Log(currentX + " " + currentY);
-    }
+    //    Debug.Log(currentX + " " + currentY);
+    //}
 }
