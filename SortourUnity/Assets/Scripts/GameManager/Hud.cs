94292fd430d0cdda20e6d307e7bea2b005a64632
Taisia Klin <taisiaklin@gmail.com>
2025-06-23 09:59:46 +0200

    UI eingebaut
    Sprites für alle Buttons und abwechselndes UI für beide Teams eingebaut

--- SortourUnity/Assets/Scripts/GameManager/Hud.cs
+++ SortourUnity/Assets/Scripts/GameManager/Hud.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class Hud : MonoBehaviour
 {
+    public GameObject WhiteMana;
+    public GameObject BlackMana;
     public TMP_Text player1ManaText;
     public TMP_Text player2ManaText;
 
@@ -27,16 +29,16 @@ public class Hud : MonoBehaviour
         if (currentPlayer == 1)
         {
             // Zeige nur das Mana von Spieler 1 an
-            player1ManaText.text = $"Spieler 1 Mana: {player1Mana} / {maxMana}";
-            player1ManaText.gameObject.SetActive(true); 
-            player2ManaText.gameObject.SetActive(false); 
+            player1ManaText.text = $"{player1Mana} / {maxMana}";
+            WhiteMana.gameObject.SetActive(true); 
+            BlackMana.gameObject.SetActive(false); 
         }
         else if (currentPlayer == 2)
         {
             // Zeige nur das Mana von Spieler 2 an
-            player2ManaText.text = $"Spieler 2 Mana: {player2Mana} / {maxMana}";
-            player2ManaText.gameObject.SetActive(true);
-            player1ManaText.gameObject.SetActive(false); 
+            player2ManaText.text = $"{player2Mana} / {maxMana}";
+            BlackMana.gameObject.SetActive(true);
+            WhiteMana.gameObject.SetActive(false); 
         }
     }
 }
